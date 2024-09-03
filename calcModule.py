28c14c918cee6d2cddf2e4ab00579564b28fe8b4
H-D-LEE <107476384+H-D-LEE@users.noreply.github.com>
2024-09-03 21:02:46 +0900

    Add files via upload

---
+++ calcModule.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 09:03:40 2024
+
+@author: user
+"""
+
+def Add(x, y) :
+    return x + y
+    
+def Sub(x, y) : 
+    return x - y
+
+def Mul(x, y) : 
+    return x * y    
+    
+def Div(x, y) :
+    return x / y
